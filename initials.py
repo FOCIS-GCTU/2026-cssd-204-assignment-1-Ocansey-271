2b34ab209a7096272d00008a8a5dbc52282619ba
Ocansey-271 <2425401394@live.gctu.edu.gh>
2026-05-30 09:05:41 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,40 @@
+#File:initials.py
+# Description:Prints Stylized Large initials with periods using only print function and basic string.
+# Assignment: 1
+#
+#Name:Elizabeth Ocansey
+#Student ID:2425401394
+#Email: 2425401394@live.gctu.edu.gh
+# Gender:Elizabeth
+# On my honor, Elizabeth Ocansey, this programming assingment is my own work
+#and i have not provided this code to any student.
 
-def main():
-  pass #code goes here
 
+    
+def main(): 
 
+   # prints small initials anda stylized large represention of EOM
 
+    print()
+    print("...EOM") 
+
+        
+    print()
+    print("...EEEEEEEEEEEE......OOOOOOOOOOOO......MMMM...MMMM...")
+    print("...EEEEEEEEEEEE......OOOOOOOOOOOO......MMMM.MMMMMM...")
+    print("...EEEE..............OO........OO......MM.MMMM.MMM...")
+    print("...EEEE..............OO........OO......MM..MM..MMM...")
+    print("...EEEEEEEEEEEE......OO........OO......MM..MM..MMM...")
+    print("...EEEEEEEEEEEE......OO........OO......MM......MMM...")
+    print("...EEEE..............OO........OO......MM......MMM...")
+    print("...EEEE..............OO........OO......MM......MMM...") 
+    print("...EEEEEEEEEEEE**....OOOOOOOOOOOO**....MM......MMMM**")
+    print("...EEEEEEEEEEEE**....OOOOOOOOOOOO**....MM......MMMM**")
 main()
+
+   
+    
+
+    
+
+    
